b2d6bb4d6fd5c0a06c2032622fde48d88ba515ed
Alvin Yang <alvin.yang73@gmail.com>
2019-02-21 17:26:54 -0800

    Updates

---
+++ precache-manifest.8c70359cc3e10f18c3fde99b29beba6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe5a6f742cc1e01ca33",
+    "url": "/rosh-calc/static/js/runtime~main.3fe5a6f7.js"
+  },
+  {
+    "revision": "38eed2c0bef005d23939",
+    "url": "/rosh-calc/static/js/main.38eed2c0.chunk.js"
+  },
+  {
+    "revision": "6f2dddf2dce2a1d893a8",
+    "url": "/rosh-calc/static/js/2.6f2dddf2.chunk.js"
+  },
+  {
+    "revision": "38eed2c0bef005d23939",
+    "url": "/rosh-calc/static/css/main.7cafccb4.chunk.css"
+  },
+  {
+    "revision": "6f2dddf2dce2a1d893a8",
+    "url": "/rosh-calc/static/css/2.b1d3aa53.chunk.css"
+  },
+  {
+    "revision": "e1489b2950cf4d42ed35f739b156a743",
+    "url": "/rosh-calc/index.html"
+  }
+];
